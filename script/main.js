e4350cd1770c0b43d61cda2eeba24d82e70db28d
jooyoung <jooyoung.cairo@gmail.com>
2022-09-22 12:56:46 +0900

    item-details popup finished

--- script/main.js
+++ script/main.js
@@ -1,3 +1,15 @@
-$(function(){
-	
+$(function() {
+	//$ :찾아라 
+	//.click(function(){~~~}) : 클릭하면 처리할 코드 
+	//.show() , .hide() : effect method
+	$(function() {
+		$("#detail").click(function() {
+			$(".item-detail").removeClass("d-none"); //.removeClass() :클래스 제거메소드
+			//.show("slow");
+		})
+		$("#close").click(function() {
+			$(".item-detail").addClass("d-none"); //.addClass() :클래스 추가소드
+			//.hide("fast");
+		})
+	})
 })
