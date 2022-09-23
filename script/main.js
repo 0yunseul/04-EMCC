a078cccd055f2b36892d8f286e9b540323217b54
jooyoung <jooyoung.cairo@gmail.com>
2022-09-23 12:50:31 +0900

    javascript add
    popup-show/hide/clss add&remove/atrtribute get&set

--- script/main.js
+++ script/main.js
@@ -2,14 +2,47 @@ $(function() {
 	//$ :찾아라 
 	//.click(function(){~~~}) : 클릭하면 처리할 코드 
 	//.show() , .hide() : effect method
-	$(function() {
-		$("#detail").click(function() {
-			$(".item-detail").removeClass("d-none"); //.removeClass() :클래스 제거메소드
-			//.show("slow");
-		})
-		$("#close").click(function() {
-			$(".item-detail").addClass("d-none"); //.addClass() :클래스 추가소드
-			//.hide("fast");
-		})
-	})
+//	$(function() {
+//		$("#detail").click(function() {
+//			$(".item-detail").removeClass("d-none"); //.removeClass() :클래스 제거메소드
+//			//.show("slow");
+//		})
+//		$("#close").click(function() {
+//			$(".item-detail").addClass("d-none"); //.addClass() :클래스 추가소드
+//			//.hide("fast");
+//		})
+//	})
+		window.addEventListener("DOMContentLoaded",function(){
+						
+						const closeBT = document.querySelector("#close");
+						const openBT = document.querySelector("#open");
+						const itemDIV = document.querySelector(".item-detail");
+					
+						
+						openBT.addEventListener("click", function(){
+							itemDIV.classList.remove("d-none");
+						});
+						closeBT.addEventListener("click", function(){
+							itemDIV.classList.add("d-none");
+						});
+				
+						const smallIMG = document.querySelectorAll(".small-img li");	//NodeList로 반환
+						const img1= document.querySelector("#img1");
+					
+						smallIMG[0].addEventListener("mouseenter", function(){ 
+							img1.setAttribute("src","https://picsum.photos/id/1/2000/2000")
+						})				
+						
+						smallIMG[1].addEventListener("mouseenter", function(){
+							img1.setAttribute("src","https://picsum.photos/id/10/2000/2000")
+						})				
+						
+						smallIMG[2].addEventListener("mouseenter", function(){
+							img1.setAttribute("src","https://picsum.photos/id/100/2000/2000")
+						})				
+						/* smallIMG.forEach(function(){ 
+							console.log("클릭했습니다")
+						})	*/				
+					
+					})
 })
